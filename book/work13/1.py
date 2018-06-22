d4d58147042b1fde923dd99b820522aafc8e0c66
tangbinbin <tangbinbin32@163.com>
2018-06-22 21:48:52 +0800

    book work

---
+++ book/work13/1.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+'''
+    编写一个函数，用大写字母打印你的名字，就像这样：
+        CCCC A   RRRRR TTTTTTTTT EEEEEE RRRRR
+        C C AA R R T E R R C   A A R R T EEEE R R C     AAAAAAAA RRRRR T E   RRRRR
+        C  C A  A R R T E  R R CCCC A    A R     R T   EEEEEE R R
+    编写一个程序多次调用这个函数。
+'''
---
+++ book/work15/2.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+'''
+    修改代码清单15-2中的代码，把c_to_f()包含到主程序的命名空间里。也就是说，修改这个代码，从而可以写：
+        fohrenheit = c_to_f(celsius)
+    而不是
+        fahrenheit = my_module.c_to_f(celsius)
+'''
